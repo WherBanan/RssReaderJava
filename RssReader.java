ff39ee8b1ba08d2fb9482edcb7b8c41326ead317
WherBanan <121439318+WherBanan@users.noreply.github.com>
2024-01-30 20:37:38 +0200

    start

--- RssReader.java
+++ RssReader.java
@@ -1,1 +1,8 @@
+import java.io.*;
+import java.net.*;
 
+public class RssReader {
+    URL rssUrl = new URL("https://www.unica.fi/modules/MenuRss/MenuRss/CurrentWeek?costNumber=1970&language=fi");
+
+    
+}
